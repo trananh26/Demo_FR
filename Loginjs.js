7387989ecf65bfb08419e576b3babc3bf0f04c81
Tuan Anh <trananh260697@gmail.com>
2023-04-05 17:30:16 +0700

    link to backend
    link to backend

--- Loginjs.js
+++ Loginjs.js
@@ -1,26 +1,28 @@
+
 $(document).ready(function() { // bắt đầu vào trang thì làm gì đó
-    alert("Mời đăng nhập ");
-    
-    $("#btnLogin").click(function() { // $
-        
-          axios.get('https://jsonplaceholder.typicode.com/todos/1') // call API method GET
-            .then(function (response) {
-                console.log(response.data); 
-                
-                if(response.data.id == 1){
-                    window.location.href = './Home.html'; //điều hướng trang
-                    alert("Đăng nhập thành công");  //hiển thị popup thông báo               
-                }
-                else {
-                    alert("Đăng nhập thất bại");  //hiển thị popup thông báo
-                };
-            })
-            .catch(function (error) {
-                console.log(error);
-            });  
-    });
-        
-    
+  alert("Mời đăng nhập ");
 
-});
+})
+  
+  function Login(){ // $
+ 
+      var username = document.getElementById("User").value;
+      var password = document.getElementById("Pass").value;
+    
+      console.log("user = " + username + " pass = " + password)
 
+      $.ajax({
+        url: "http://localhost:3000/api/v1/account/login",
+        method: "POST",
+        data: { UserName: username, PassWord: password }
+      })
+      .then(response => {
+          console.log(response.data); 
+          alert("Đăng nhập thành công");  //hiển thị popup thông báo     
+          window.location.href = './Home'; //điều hướng trang                  
+        })
+        .catch(err => {
+          console.log(err.responseJSON)
+          alert(err.responseJSON);  //hiển thị popup thông báo 
+        })
+      };    
