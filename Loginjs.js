682eaebbc501356b694ab0ba1bbba1f03d1751e3
Tuan Anh <trananh260697@gmail.com>
2023-04-13 19:59:56 +0700

    login
    login

--- Loginjs.js
+++ Loginjs.js
@@ -1,28 +1,22 @@
-
-$(document).ready(function() { // bắt đầu vào trang thì làm gì đó
-  alert("Mời đăng nhập ");
-
-})
-  
-  function Login(){ // $
+function Login(){ // $
  
-      var username = document.getElementById("User").value;
-      var password = document.getElementById("Pass").value;
-    
-      console.log("user = " + username + " pass = " + password)
+  var username = document.getElementById("User").value;
+  var password = document.getElementById("Pass").value;
 
-      $.ajax({
-        url: "http://localhost:3000/api/v1/account/login",
-        method: "POST",
-        data: { UserName: username, PassWord: password }
-      })
-      .then(response => {
-          console.log(response.data); 
-          alert("Đăng nhập thành công");  //hiển thị popup thông báo     
-          window.location.href = './Home'; //điều hướng trang                  
-        })
-        .catch(err => {
-          console.log(err.responseJSON)
-          alert(err.responseJSON);  //hiển thị popup thông báo 
-        })
-      };    
+  console.log("user = " + username + " pass = " + password)
+
+  $.ajax({
+    url: "http://localhost:3000/api/v1/account/login",
+    method: "POST",
+    data: { UserName: username, PassWord: password }
+  })
+  .then(response => {
+      console.log(response.data); 
+      alert("Đăng nhập thành công");  //hiển thị popup thông báo     
+      window.location.href = './Home.html'; //điều hướng trang                  
+    })
+    .catch(err => {
+      console.log(err.responseJSON)
+      alert(err.responseJSON);  //hiển thị popup thông báo 
+    })
+  };    
